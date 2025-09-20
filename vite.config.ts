31a78eb1074f0f287c8eb47cdd2f24e506595c1f
fdota <fdotautorino@gmail.com>
2025-09-20 21:12:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./public/index.html"
+    }
+  }
 }));
